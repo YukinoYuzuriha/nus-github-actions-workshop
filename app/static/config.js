7b11c36098f1ddac919cdb9a7c842a82286856b2
YukinoYuzuriha <144689069+YukinoYuzuriha@users.noreply.github.com>
2026-03-24 19:23:33 +0800

    re edit

--- app/static/config.js
+++ app/static/config.js
@@ -7,7 +7,7 @@ var CONFIG = {
 
     items: [
         { type: "commit", color: "#238636", pts: 10, weight: 50, sym: "\u2713" },
-        { type: "25",     color: "#58a6ff", pts: 25, weight: 25, sym: "PR" },
+        { type: "25",     color: "#58a6ff", pts: 25, weight: 25, sym: "25" },
         { type: "50",   color: "#e3b341", pts: 50, weight: 5,  sym: "\u2605" },
         { type: "bug",    color: "#f85149", pts: 0,  weight: 20, sym: "\u2715", bad: true }
     ],
